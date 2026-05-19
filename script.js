0facf88fb42d840ab671a672e15c897459d12cb5
Torolero <42136673+Torolero@users.noreply.github.com>
2026-05-19 12:35:44 -0700

    Create rows and columns
    Created the rows and columns with flexbox using nested
    for loops.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /*Create divs with js to go inside div with ID "container" */
+const containerDiv = document.getElementById("container");
+
+for (let i = 0; i < 16; i++) {
+const row = document.createElement("div");
+containerDiv.appendChild(row);
+row.classList.add("row");
+
+for (let i = 0; i < 16; i++) {
+
+const column = document.createElement("div");
+row.appendChild(column);
+column.classList.add("column");
+column.textContent = "column";
+}
+}
+
+
+
