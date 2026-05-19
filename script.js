be73fa264c156e39496f3bc0df09950b82b10bfa
Torolero <42136673+Torolero@users.noreply.github.com>
2026-05-19 13:26:37 -0700

    Create button and add styling to it
    Created a button at the top and added styling to it.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 /*Create divs with js to go inside div with ID "container" */
+const body = document.querySelector("body");
+const  button = document.createElement("button");
+body.prepend(button);
+button.textContent = "Click Me!!";
+button.classList.add("buttonStyling");
+button.addEventListener('mouseenter', (event) => {
+    button.classList.add("hoverButton");
+
+    button.addEventListener('mouseleave', (event) => {
+        button.classList.remove("hoverButton");
+    });
+});
+
+button.addEventListener('click', (event) => {
+    let userInput = prompt("Enter a number");
+});
+
+
+
 const containerDiv = document.getElementById("container");
 
 for (let i = 0; i < 16; i++) {
